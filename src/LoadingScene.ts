253f99a4e2573d82b96d46a95f9b61bed39c6138
unknown <brychan.thomas@btinternet.com>
2020-10-04 13:30:46 +0100

    Created LoadingScene

---
+++ src/LoadingScene.ts
@@ -0,0 +1,28 @@
+import { MultiplayerHandler } from './multiplayer.js';
+
+export class LoadingScene extends Phaser.Scene {
+
+  private multiplayerHandler = new MultiplayerHandler();
+
+  constructor() {
+    super('loading');
+  }
+
+  preload() {}
+
+  create() {
+    this.add.text(100, 100, "Loading...");
+    let address = (<HTMLInputElement>document.getElementById('serverAddress')).value;
+    let lobbyCode = (<HTMLInputElement>document.getElementById('lobbyCode')).value;
+    let username = (<HTMLInputElement>document.getElementById('username')).value;
+
+    this.multiplayerHandler = new MultiplayerHandler();
+    this.multiplayerHandler.join(address, lobbyCode, username)
+      .then(function() {
+        this.scene.start('holdingArea', {multiplayerHandler: this.multiplayerHandler});
+      }.bind(this))
+      .catch(function() {
+        this.scene.start('mainMenu');
+      }.bind(this));
+  }
+}
