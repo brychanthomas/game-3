372f7237d05796e94655b8be7faccbfc596aaac7
unknown <brychan.thomas@btinternet.com>
2020-10-28 19:44:37 +0000

    Created MessageTemplate which is used by Gson to decode JSON

---
+++ game-3-server/src/com/gameServer/MessageTemplate.java
@@ -0,0 +1,21 @@
+package com.gameServer;
+
+import java.util.HashMap;
+
+/** Class for creating objects representing message from client, used to decode JSON */
+public class MessageTemplate {
+	public int type;
+	public int id;
+	public String lobbyCode;
+	public String username;
+	public double velocityX;
+	public double velocityY;
+	public double x;
+	public double y;
+	public HashMap<String, Integer> properties;
+	
+	public String toString() {
+		return "type: "+type+", id: "+id+", lobbyCode: "+lobbyCode+", username: "+username
+				+", properties: "+properties;
+	}
+}
